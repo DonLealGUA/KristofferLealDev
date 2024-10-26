f393c81a0737e77fb3431ae04011997923ea233d
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2024-10-26 16:44:34 +0200

    Start code for Contact/Resume pages

---
+++ my-app/src/Pages/Contact.jsx
@@ -0,0 +1,13 @@
+// src/Pages/Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>This is the Contact page.</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ my-app/src/Pages/Resume.jsx
@@ -0,0 +1,13 @@
+// src/Pages/Resume.js
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>This is the Resume page.</p>
+    </div>
+  );
+};
+
+export default Resume;
