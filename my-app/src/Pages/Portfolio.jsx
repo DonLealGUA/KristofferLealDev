840fdb9079bb11a4b9ef1e9314e852c794dc039f
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2024-10-27 18:36:18 +0100

    updated Portfolio

--- my-app/src/Pages/Portfolio.jsx
+++ my-app/src/Pages/Portfolio.jsx
@@ -9,34 +9,41 @@ import { BackgroundBeams } from '../componentes/ui/AceternityUI/background-beams
 const Portfolio = () => {
   const projects = [
     {
-        title: "PuzzleQuest",
-        description: "PuzzleQuest is a pixel art RPG for all ages, featuring quests and exploration. Developed in Java without a game engine, it emphasizes creativity and accessibility with environmental sounds, character movement, and interactive NPCs. Players solve puzzles to unlock new areas, all while enjoying a smooth gameplay loop on Windows.",
+        title: "HomeQuest",
+        description: "HomeQuest is a pixel art RPG for all ages, featuring quests and exploration. Developed in Java without a game engine, it emphasizes creativity and accessibility with environmental sounds, character movement, and interactive NPCs. Players solve puzzles to unlock new areas, all while enjoying a smooth gameplay loop on Windows.",
         link: "https://github.com/harnesko/PuzzleQuest",
+        imgURL: "/assets/Portfolio/HomeQuest.jpg",
     },
     {
         title: "Chatroom",
         description: "Java Chatroom is a real-time messaging platform with public and private chatrooms. Built using Java sockets, it features a GUI with user authentication, member lists, and direct messaging capabilities, designed to run with Java 8 or later on Windows.",
         link: "https://github.com/DonLealGUA/Chatroom",
+        imgURL: "/assets/Portfolio/Chatroom.jpg",
     },
     {
         title: "JediArchives",
         description: "JediArchives is a React site for Star Wars fans to explore characters using the SWAPI API. Styled with Chakra UI, it provides a seamless interface for users to dive into Star Wars lore and discover details about their favorite characters.",
         link: "https://github.com/DonLealGUA/JediArchives",
+        imgURL: "/assets/Portfolio/Jedi.jpg",
     },
     {
         title: "OogHub",
         description: "OogHub is a React app that offers users random quotes inspired by Master Oogway. Users can save quotes and learn about their authors, leveraging the Quotable and Wikipedia APIs for insightful content, enhanced by smooth animations and a responsive layout.",
         link: "https://github.com/YunboChow/OogHub",
+        imgURL: "/assets/Portfolio/OogHub.jpg",
+        weblink: "https://ooghub.vercel.app/", 
     },
     {
         title: "BlackJack",
         description: "The Blackjack Game is a C# application using Windows Forms and Entity Framework Core, bringing the classic card game to life. Players enjoy traditional gameplay with data persistence in a SQL Server database, managed through LINQ for efficient interactions.",
         link: "https://github.com/DonLealGUA/BlackJack",
+        imgURL: "/assets/Portfolio/BlackJack.jpg",
     },
     {
         title: "ScreenScore",
         description: "ScreenScore is a React-based movie management app that lets users add, delete, and organize movies. It features a user-friendly interface, responsive design, and allows sorting by title or rating, enhancing the experience of managing a personal collection.",
         link: "https://github.com/DonLealGUA/ScreenScore",
+        imgURL: "/assets/Portfolio/ScreenScore.jpg",
     },
 ];
 
@@ -74,9 +81,11 @@ const Portfolio = () => {
             {projects.map((project, index) => (
               <Card
                 key={index}
+                imageUrl={project.imgURL}
                 title={project.title}
                 description={project.description}
                 link={project.link}
+                weblink={project.weblink}
               />
             ))}
           </div>
--- my-app/src/componentes/ProjectCard.jsx
+++ my-app/src/componentes/ProjectCard.jsx
@@ -1,23 +1,32 @@
 // src/Components/Card.js
 import React from 'react';
 import './ui/CSS/Card.css'; 
-import { FaGithub } from 'react-icons/fa'; 
+import { FaGlobe, FaGithub } from 'react-icons/fa'; 
 import { HoverBorderGradient } from './ui/AceternityUI/hover-border-gradient'; 
 
-const Card = ({ title, description, link }) => {
+const Card = ({ title, description, imageUrl, link, weblink }) => {
   return (
     <div className="Card">
-      <h2 className="CardTitle bold-text text-xl mb-3">{title}</h2>
+      {imageUrl && <img src={imageUrl} alt={title} className="CardImage" />}
+      
+      <h2 className="CardTitle">{title}</h2>
       <p className="CardDescription">{description}</p>
+      
       <div className="ButtonContainer">
+        {weblink && (
+          <FaGlobe
+            className="GlobeIcon"
+            onClick={() => window.open(weblink)} // Opens the web link
+          />
+        )}
         <HoverBorderGradient
           containerClassName="rounded-full"
           as="button"
           className="dark:bg-black text-black dark:text-white flex items-center space-x-2 p-2"
-          onClick={() => window.open(link)} 
+          onClick={() => window.open(link)} // Opens the GitHub link
         >
-             <FaGithub className="text-xl text-white" /> 
-          <span className='text-white'>Check out the project</span>
+          <FaGithub className="text-xl text-white" />
+          <span className="text-white">Check out the project</span>
         </HoverBorderGradient>
       </div>
     </div>
